1d4dfe036d7e6967a9d7819994ca0f4892a1575e
Andrew <apjt66@gmail.com>
2021-09-09 16:19:44 -0700

    Added controller and tempdb

---
+++ src/controllers/recipeController.js
@@ -0,0 +1,7 @@
+const getAll = (req, res) => {
+    res.send("d");
+}
+
+module.exports = {
+    getAll
+}
--- src/server.js
+++ src/server.js
@@ -1,13 +1,12 @@
 const express = require("express");
 
 const app = express();
-const recipeRoute = require('./route/recipeRoute');
+const recipeRoute = require('./routes/recipeRoute');
 
 app.use(express.urlencoded({extended: true})); 
 app.use(express.json());
 
 app.use('/recipe', recipeRoute)
-
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000.");
