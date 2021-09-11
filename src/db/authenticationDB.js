9b469284b41efccaedc6e5acb11efe458544af32
Andrew <apjt66@gmail.com>
2021-09-11 05:26:29 -0700

    changed file caps

--- src/db/authenticationDB.js
+++ src/db/authenticationDB.js
@@ -1,4 +1,4 @@
-const User = require('../models/User');
+const User = require('../models/user');
 
 const signup = async (username, password) => {
     let userExists = await User.exists({username});
